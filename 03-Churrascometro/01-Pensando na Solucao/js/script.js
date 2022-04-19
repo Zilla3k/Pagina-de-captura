e132fb6832fed788a5010a241d0084e1fd42134f
zilla3k <henriquepelanda.web@gmail.com>
2022-04-19 08:30:55 -0300

    new file

--- 03-Churrascometro/01-Pensando na Solucao/js/script.js
+++
@@ -1,4 +0,0 @@
-// Carne - 400gr por pessoa + de 6 horas - 650gr
-// Cerveja - 1200ml por pessoa + de 6 horas - 2000ml
-// Refrigerante/agua - 1000ml por pessoa + 6 horas - 1500ml
-// criancas valem por metade
